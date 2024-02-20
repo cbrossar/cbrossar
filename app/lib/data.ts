2144841efb04d93de2c1ea29ee52f88a57aeb1ec
Cole Brossart <cole.brossart@gmail.com>
2024-02-20 09:39:48 -0500

    constants

---
+++ app/lib/data.ts
@@ -0,0 +1,10 @@
+
+export function fetchAlbumReviews() {
+
+    return [{
+        "name": "Flower boy",
+        "artist": "Tyler, the Creator",
+        "rating": 10,
+        "image_url": "flower-boy.jpg"
+    }]
+}
