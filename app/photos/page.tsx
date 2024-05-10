4d162765a54bf720885de7d9eaa4f69424e0b864
Cole Brossart <cole.brossart@gmail.com>
2024-05-10 17:08:23 -0400

    photos

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import styled from "styled-components";
+import Image from "next/image";
+
 export default function Page() {
-    return <p>Photos Page</p>;
+
+    const photos = [
+        "got-slushed.jpeg",
+        "bottles.jpeg",
+        "casino.jpeg",
+        "goodboy.jpeg",
+        "londoneye.jpeg",
+        "strawberries.jpeg",
+        "bigtree.jpeg",
+        "rondo.jpeg",
+        "rue.jpeg",
+        "the-square.jpeg",
+        "creek.jpeg",
+        "penny-farthing.jpeg",
+        "rose-du-pont.jpeg",
+        "spices.jpeg",
+        "street-lamp.jpeg",
+    ];
+
+    return (
+        <PhotosWrapper>  
+            {photos.map((photo, index) => (
+                <ImageWrapper key={index}>
+                    <Image
+                        src={`/photos/${photo}`}
+                        width={900}
+                        height={450}
+                        alt={`Photo ${index}`}
+                    />
+                </ImageWrapper>
+            ))}
+        </PhotosWrapper>
+    );
 }
+
+const ImageWrapper = styled.div`
+    position: relative;
+    margin-bottom: 20px;
+`;
+
+
+const PhotosWrapper = styled.div``;
