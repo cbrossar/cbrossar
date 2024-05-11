74117b08b3505cce1b3133ee510dcfad63d9f871
Cole Brossart <cole.brossart@gmail.com>
2024-05-11 15:45:59 -0400

    fmt

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,7 +5,6 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { put } from "@vercel/blob";
 
-
 const FormSchema = z.object({
     id: z.string(),
     album: z.string({
