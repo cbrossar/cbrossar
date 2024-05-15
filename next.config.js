f6c50bc55e54547e65c599d87cf9570f8c4eb4b2
Cole Brossart <cole.brossart@gmail.com>
2024-05-14 23:36:08 -0400

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
     images: {
-        domains: ["k2btciufj0chkpse.public.blob.vercel-storage.com"],
+        domains: ["k2btciufj0chkpse.public.blob.vercel-storage.com", "cbrossar-website.s3.amazonaws.com"],
     },
 };
