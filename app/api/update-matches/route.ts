24667df39c7181071bd35f20fd29c1afecaf16ad
Cole Brossart <cole.brossart@gmail.com>
2024-05-26 17:59:28 -0400

    update the route

--- app/api/update-matches/route.ts
+++ app/api/update-matches/route.ts
@@ -15,18 +15,21 @@ export async function GET(request: Request) {
         // Load the HTML content into Cheerio
         const $ = cheerio.load(response.data);
 
-        if(!$) {
+        if (!$) {
             return new Response("Failed to load the webpage", { status: 500 });
         }
-
-        // Extract data from the webpage (example: title)
-        const title = $("title").text();
 
         // Iterate over each .schedule-match element
         $(".schedule-match").each((index, element) => {
             const matchDate = $(element).attr("data-date") ?? "";
-            const awayScore = parseInt($(element).attr("data-away_score") ?? '0', 0);
-            const homeScore = parseInt($(element).attr("data-home_score") ?? '0', 0);
+            const awayScore = parseInt(
+                $(element).attr("data-away_score") ?? "0",
+                0,
+            );
+            const homeScore = parseInt(
+                $(element).attr("data-home_score") ?? "0",
+                0,
+            );
 
             console.log(matchDate, homeScore, awayScore);
 
@@ -52,13 +55,15 @@ export async function GET(request: Request) {
                 awayScore,
                 matchDate,
             );
+
+            console.log("Attempted to create match.");
         });
         createMatchUpdate(true);
 
         // Send back the extracted data as a response
-        return Response.json({ title });
+        return new Response("Successfully crawled the webpage.");
     } catch (error) {
         createMatchUpdate(false);
-        return Response.json({ error: "Failed to crawl the webpage." });
+        return new Response("Failed to crawl the webpage.", { status: 500 });
     }
 }
