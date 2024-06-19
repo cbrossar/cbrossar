4189fe55c09998c37264df371503b57120c98ac5
Cole Brossart <cole.brossart@gmail.com>
2024-06-19 00:13:27 -0400

    resolve bot detection

--- app/api/bethpage/route.ts
+++ app/api/bethpage/route.ts
@@ -8,18 +8,27 @@ export async function GET(request: Request) {
         const browser = await puppeteer.launch({ headless: true }); // Set headless: true for production
         const page = await browser.newPage();
 
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36');
+
         // Navigate to the login page
         await page.goto(bethpage_url); // Replace with actual login URL
 
+        await page.screenshot({ path: "screenshot1.png" }); // Change the path as needed
+
         // Click the "Resident" button
+        await page.waitForSelector(".booking-classes .btn-primary:nth-of-type(1)");
         await page.click(".booking-classes .btn-primary:nth-of-type(1)");
 
         // Wait for the login button to be available and then click it
         await page.waitForSelector(".btn.btn-lg.btn-primary.login");
         await page.click(".btn.btn-lg.btn-primary.login");
 
+
         // Wait for the login form to appear
         await page.waitForSelector("#login_email"); // Wait for the email field to be available
+
+        await page.screenshot({ path: "screenshot2.png" }); // Change the path as needed
+
 
         // Enter login credentials
         await page.type("#login_email", process.env.BETHPAGE_EMAIL as string); // Replace with your email
