c30fbb1b8fd47b06dc985253295ae1079f310338
Cole Brossart <cole.brossart@gmail.com>
2024-06-19 23:27:38 -0400

    works!!

--- app/api/bethpage/route.ts
+++ app/api/bethpage/route.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 60; // This function can run for a maximum of 5 seconds
+export const maxDuration = 60;
 
 import puppeteerCore from "puppeteer-core";
 import puppeteer from "puppeteer";
