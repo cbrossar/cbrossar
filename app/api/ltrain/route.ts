6136eedf591b29f33bdafa439aa8d75e69389a37
Cole Brossart <cole.brossart@gmail.com>
2024-06-30 22:19:46 -0400

    attempt

--- app/api/ltrain/route.ts
+++ app/api/ltrain/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: Request) {
         const response = await fetch(MTA_API_URL);
 
         const buffer = await response.arrayBuffer();
-        const root = await protobuf.load("public/ltrain/gtfs-realtime.proto"); // Adjust the path to the GTFS-Realtime .proto file
+        const root = await protobuf.load("ltrain/gtfs-realtime.proto");
         const FeedMessage = root.lookupType("transit_realtime.FeedMessage");
 
         const message = FeedMessage.decode(new Uint8Array(buffer));
