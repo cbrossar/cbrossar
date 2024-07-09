350e67c1d75e96763d7b387dd0ebcb3a8415872d
Cole Brossart <cole.brossart@gmail.com>
2024-07-08 22:17:38 -0400

    improve text

--- app/ltrain/page.tsx
+++ app/ltrain/page.tsx
@@ -37,7 +37,7 @@ export default function Page() {
     return (
         <div className="flex flex-col items-center">
             <h1 className="text-3xl font-bold mb-6">L Train</h1>
-            <h2 className="text-2xl mb-6">Bedford Ave - Manhattan Bound</h2>
+            <h2 className="text-xl mb-6">Bedford Ave - Manhattan Bound</h2>
             <ul className="bg-white shadow-md rounded-lg p-4 w-full max-w-md">
                 {lTrainTimes.map((time, i) => {
                     const { minutesUntil, trainDatetime } =
@@ -49,7 +49,7 @@ export default function Page() {
                             key={i}
                             className="border-b last:border-b-0 py-2 text-gray-800 text-center"
                         >
-                            <Tooltip 
+                            <Tooltip
                                 title={fullTime}
                                 enterTouchDelay={0} // Show tooltip immediately on touch
                                 leaveTouchDelay={1500} // Keep tooltip open for a while after touch ends
