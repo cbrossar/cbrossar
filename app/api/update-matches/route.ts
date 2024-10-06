1fe93f2cb121090c2f0209ff524a72a991d4163f
Cole Brossart <cole.brossart@gmail.com>
2024-10-06 09:52:27 -0400

    Garnet

--- app/api/update-matches/route.ts
+++ app/api/update-matches/route.ts
@@ -1,12 +1,14 @@
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 
 import updateBeermenMatches from "./update-beermen-matches";
+import updateGarnetMatches from "./update-garnet-matches";
 import updateHotspurMatches from "./update-hotspur-mtaches";
 
 export async function GET(request: Request) {
     try {
         await updateBeermenMatches();
         await updateHotspurMatches();
+        await updateGarnetMatches();
 
         // Send back the extracted data as a response
         return new Response("Successfully updated matches.");
---
+++ app/api/update-matches/update-garnet-matches.ts
@@ -0,0 +1,136 @@
+import puppeteerCore from "puppeteer-core";
+import puppeteer from "puppeteer";
+import chromium from "@sparticuz/chromium";
+import { createMatch, createTeam, createMatchUpdate } from "@/app/lib/data";
+
+export const dynamic = "force-dynamic";
+
+async function getBrowser() {
+    if (process.env.VERCEL_ENV === "prod") {
+        const executablePath = await chromium.executablePath();
+
+        const browser = await puppeteerCore.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath,
+            headless: chromium.headless,
+        });
+        return browser;
+    } else {
+        const browser = await puppeteer.launch({ headless: true });
+        return browser;
+    }
+}
+
+export default async function updateGarnetMatches() {
+    try {
+        const url =
+            "https://metrosoccerny.leagueapps.com/leagues/4374844/teams/7033425";
+
+        const browser = await getBrowser();
+        const page = await browser.newPage();
+
+        // Set user agent
+        await page.setUserAgent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+        );
+
+        // Navigate to the URL and wait for the page to load
+        await page.goto(url, { waitUntil: "networkidle2" });
+
+        // Wait for the iframe to load
+        await page.waitForSelector("iframe#monolith-iframe");
+
+        // Get the iframe element
+        const iframeElement = await page.$("iframe#monolith-iframe");
+
+        // Switch context to the iframe
+        const iframe = await iframeElement.contentFrame();
+
+        // Wait for the schedule-game elements to appear inside the iframe
+        await iframe.waitForSelector(".schedule-game");
+
+        // Scrape the matches inside the iframe
+        const matches = await iframe.$$eval(".schedule-game", (elements) => {
+            return elements
+                .map((element) => {
+                    const matchDate =
+                        element
+                            .querySelector(".sched-start-date .date")
+                            ?.textContent?.trim() || "";
+                    const matchTime =
+                        element
+                            .querySelector(".sched-start-date .time")
+                            ?.textContent?.trim() || "";
+
+                    const dateParts = matchDate.split(", ")[1];
+                    const fullDateStr = `${dateParts} ${new Date().getFullYear()} ${matchTime}`; // Add current year
+                    const fullDate = new Date(fullDateStr).toISOString();
+
+                    const opposingTeamName =
+                        element
+                            .querySelector("h3.event-team a:first-child")
+                            ?.textContent?.trim() || "";
+                    const isHome =
+                        element
+                            .querySelector("h3.event-team tag")
+                            ?.textContent?.trim() === "(H)";
+                    const homeTeamName = isHome
+                        ? "Garnet United"
+                        : opposingTeamName;
+                    const awayTeamName = isHome
+                        ? opposingTeamName
+                        : "Garnet United";
+
+                    // Substring to remove W/L/D from the score
+                    const scoreText = element
+                        .querySelector(".team-result")
+                        ?.textContent?.trim()
+                        .substring(1);
+
+                    if (scoreText) {
+                        const [homeScore, awayScore] = scoreText
+                            .split("-")
+                            .map((score) => parseInt(score.trim(), 10));
+
+                        return {
+                            fullDate,
+                            homeTeamName,
+                            awayTeamName,
+                            homeScore,
+                            awayScore,
+                        };
+                    }
+
+                    // Return null if no score is found
+                    return null;
+                })
+                .filter((match) => match !== null); // Filter out matches without a score
+        });
+
+        // Iterate over the matches and create them
+        for (const match of matches) {
+            if (match.homeTeamName && match.awayTeamName) {
+                await createTeam(match.homeTeamName);
+                await createTeam(match.awayTeamName);
+                await createMatch(
+                    match.homeTeamName,
+                    match.awayTeamName,
+                    match.homeScore,
+                    match.awayScore,
+                    match.fullDate,
+                );
+            }
+        }
+
+        // Update the match process status to true if successful
+        await createMatchUpdate(true);
+
+        // Close the browser
+        await browser.close();
+    } catch (error) {
+        // If an error occurs, log it and mark update as failed
+        // await crea?teMatchUpdate(false);
+        console.error("Failed to fetch garnet matches", error);
+    }
+}
