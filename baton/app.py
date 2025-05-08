9f951c46837c4a2c80e96aa5a2d38183de391c13
Cole Brossart <cole.brossart@gmail.com>
2025-05-07 21:58:17 -0400

    try this

--- baton/app.py
+++ baton/app.py
@@ -4,23 +4,20 @@ from player_gameweeks import run_player_gameweeks
 from last_5 import run_last_5_points
 from logger import logger
 from emails import send_email
-import os
 import uvicorn
 
-email_to = os.getenv("EMAIL_USER")
 app = FastAPI()
 
 @app.get("/")
 async def health_check():
     try:
+        logger.info("TESTING!!!")
         success = run_health_check()
-        raise Exception("test error")
         if not success:
             raise HTTPException(status_code=500, detail="Health check failed")
         return {"status": "healthy"}
     except Exception as e:
         logger.error(f"Health check error: {str(e)}")
-        send_email("Baton: Health check failed", str(e), email_to)
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/player-gameweeks")
