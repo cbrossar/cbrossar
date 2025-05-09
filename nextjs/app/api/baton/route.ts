c5e90d5b76d72cd13d40dc17d95533aea9f4d208
Cole Brossart <cole.brossart@gmail.com>
2025-05-09 14:26:44 -0400

    baton url

---
+++ nextjs/app/api/baton/route.ts
@@ -0,0 +1,11 @@
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
+
+export async function GET(request: Request) {
+
+    const BATON_URL = process.env.BATON_URL
+
+    const response = await fetch(`${BATON_URL}`)
+    const data = await response.json()
+
+    return new Response(JSON.stringify(data))
+}
