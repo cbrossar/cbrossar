c858d56bce9a6b5c50833de5beeda679ab6ecb83
Cole Brossart <cole.brossart@gmail.com>
2025-05-09 15:40:36 -0400

    lightening speed

--- baton/players.py
+++ baton/players.py
@@ -1,4 +1,5 @@
 from db import Session
+from sqlalchemy.dialects.postgresql import insert
 from logger import logger
 from models import FantasyPlayers
 from fpl import get_fpl_general_info
@@ -24,43 +25,39 @@ def run_update_players():
 
 def update_players(data):
 
-    players_to_upsert = []
+    player_dicts = []
 
     for element in data["elements"]:
-
-        player = FantasyPlayers(
-            id=element["id"],
-            first_name=element["first_name"],
-            second_name=element["second_name"],
-            team=element["team"],
-            element_type=element["element_type"],
-            cost_change_start=element["cost_change_start"],
-            now_cost=element["now_cost"],
-            total_points=element["total_points"],
-            event_points=element["event_points"],
-            minutes=element["minutes"],
-            goals_scored=element["goals_scored"],
-            assists=element["assists"],
-            clean_sheets=element["clean_sheets"],
-            expected_goals=element["expected_goals"],
-            expected_assists=element["expected_assists"],
-            transfers_in=element["transfers_in"],
-            transfers_in_event=element["transfers_in_event"],
-        )
-
-        players_to_upsert.append(player)
+        player_dicts.append({
+            "id": element["id"],
+            "first_name": element["first_name"],
+            "second_name": element["second_name"],
+            "team": element["team"],
+            "element_type": element["element_type"],
+            "cost_change_start": element["cost_change_start"],
+            "now_cost": element["now_cost"],
+            "total_points": element["total_points"],
+            "event_points": element["event_points"],
+            "minutes": element["minutes"],
+            "goals_scored": element["goals_scored"],
+            "assists": element["assists"],
+            "clean_sheets": element["clean_sheets"],
+            "expected_goals": element["expected_goals"],
+            "expected_assists": element["expected_assists"],
+            "transfers_in": element["transfers_in"],
+            "transfers_in_event": element["transfers_in_event"],
+        })
 
     with Session() as session:
-        # Use _bulk_save_mappings with proper parameters for upsert operation
-        session._bulk_save_mappings(
-            FantasyPlayers,
-            [vars(p) for p in players_to_upsert],
-            isupdate=True,
-            isstates=False,
-            return_defaults=False,
-            update_changed_only=False,
-            render_nulls=False
+        stmt = insert(FantasyPlayers).values(player_dicts)
+        update_columns = {col.name: getattr(stmt.excluded, col.name)
+                          for col in FantasyPlayers.__table__.columns
+                          if col.name != 'id'}  # exclude PK
+        stmt = stmt.on_conflict_do_update(
+            index_elements=['id'],
+            set_=update_columns
         )
+        session.execute(stmt)
         session.commit()
 
 
