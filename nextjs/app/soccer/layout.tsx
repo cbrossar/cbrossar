d2869f4cb05bd27ca20a272ccee4c2dde9b97c15
Cole Brossart <cole.brossart@gmail.com>
2025-05-15 22:33:32 -0400

    nav css

--- nextjs/app/soccer/layout.tsx
+++ nextjs/app/soccer/layout.tsx
@@ -1,20 +1,28 @@
+"use client";
 import Link from "next/link";
 import styles from "./styles.module.css";
+import { usePathname } from "next/navigation";
 
 export default function SoccerLayout({ children }: { children: React.ReactNode }) {
+
+    const pathname = usePathname();
+    const isFantasyPremPlayers = pathname.includes("fantasy-prem/players");
+    const isFantasyPremTeam = pathname.includes("fantasy-prem/team");
+    const isMyTeams = pathname.includes("my-teams");
+
   return (
     <div>
       <nav className={styles.navBar}>
-        <Link href="/soccer/my-teams" className={styles.navLink}>
-          My Teams
-        </Link>
-        <span className={styles.navDot}>•</span>
-        <Link href="/soccer/fantasy-prem/players" className={styles.navLink}>
+        <Link href="/soccer/fantasy-prem/players" className={isFantasyPremPlayers ? styles.navLinkActive : styles.navLink}>
           Fantasy Players
         </Link>
         <span className={styles.navDot}>•</span>
-        <Link href="/soccer/fantasy-prem/team" className={styles.navLink}>
+        <Link href="/soccer/fantasy-prem/team" className={isFantasyPremTeam ? styles.navLinkActive : styles.navLink}>
           Fantasy Team
+        </Link>
+        <span className={styles.navDot}>•</span>
+        <Link href="/soccer/my-teams" className={isMyTeams ? styles.navLinkActive : styles.navLink}>
+          My Teams
         </Link>
       </nav>
       {children}
