c717967b721165c3497e97532a10481fa1b1da51
Cole Brossart <cole.brossart@gmail.com>
2025-06-09 23:52:22 -0400

    plus

--- nextjs/app/music/page.tsx
+++ nextjs/app/music/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { PlusIcon } from "@heroicons/react/24/outline";
 import { fetchMusicReviews } from "@/app/data/music";
 import Search from "@/app/ui/search";
 import styles from "./styles.module.css";
@@ -33,7 +34,7 @@ export default async function Page({
                     <Search placeholder="Search" shouldSetPage={false} />
                 </div>
                 <Link href="/music/create" className={styles.addButton}>
-                    +
+                    <PlusIcon className="h-5 w-5" />
                 </Link>
             </div>
             {musicReviews.length === 0 ? (
