2fc76d3b843b9fff5a524b9b0fb71469e6d29803
Cole Brossart <cole.brossart@gmail.com>
2025-08-19 23:09:10 -0700

    teams

--- baton/runners/teams.py
+++ baton/runners/teams.py
@@ -7,38 +7,36 @@ from logger import logger
 def run_teams():
 
     fpl_general_info = get_fpl_general_info()
-
     fpl_teams = fpl_general_info["teams"]
 
-    i = 1
+    with Session() as session:
 
-    with Session() as session: 
-
-        # update teams
         for fpl_team in fpl_teams:
             team_name = fpl_team["name"]
             team_id = fpl_team["id"]
 
-            team = session.query(FantasyTeams).filter(FantasyTeams.fpl_id == team_id).first()
+            team = (
+                session.query(FantasyTeams)
+                .filter(FantasyTeams.name == team_name)
+                .first()
+            )
+
             if team:
-                team.name = team_name
+                team.fpl_id = team_id
+                logger.info(
+                    f"Updated team {team_name} with FPL ID {team_id}, id {team.id}"
+                )
             else:
-                team = session.query(FantasyTeams).filter(FantasyTeams.name == team_name).first()
-                if team:
-                    team.fpl_id = team_id
-                    logger.info(f"Updated team {team_name} with FPL ID {team_id}")
-                else:
-                    team = FantasyTeams(
-                        id = 20 + i,
-                        name=team_name,
-                        fpl_id=team_id,
-                    )
-                    i += 1
-                    logger.info(f"Created team {team_name} with FPL ID {team_id}, id {team.id}")
+                team = FantasyTeams(
+                    name=team_name,
+                    fpl_id=team_id,
+                )
+                logger.info(
+                    f"Created team {team_name} with FPL ID {team_id}, id {team.id}"
+                )
 
             session.add(team)
 
         session.commit()
-
 
     return True
