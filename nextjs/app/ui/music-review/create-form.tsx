c93a2abcb43838160ac1df47dc9ae9af7ccc6ebf
Cole Brossart <cole.brossart@gmail.com>
2025-10-04 10:39:59 -0700

    betta

--- nextjs/app/ui/music-review/create-form.tsx
+++ nextjs/app/ui/music-review/create-form.tsx
@@ -550,7 +550,7 @@ export default function Form() {
                     )}
 
                     {/* File upload - only show if not using MusicBrainz cover art */}
-                    {selectedAlbum && !(coverArtUrl && useCoverArt) && (
+                    {selectedAlbum && !isLoadingCoverArt && !(coverArtUrl && useCoverArt) && (
                         <div>
                             <label
                                 htmlFor="image_file"
