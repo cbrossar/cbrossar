94c49957b1246fb3b546d5bbea2dbd461fa7081a
Cole Brossart <cole.brossart@gmail.com>
2025-12-13 20:47:22 -0800

    .env setup

--- baton/app.py
+++ baton/app.py
@@ -1,8 +1,14 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
 import uvicorn
 import db
 from fastapi import FastAPI
 from routes import router
+
+env_path = Path(__file__).parent / ".env"
+if env_path.exists():
+    load_dotenv(env_path)
 
 app = FastAPI()
 app.include_router(router)
